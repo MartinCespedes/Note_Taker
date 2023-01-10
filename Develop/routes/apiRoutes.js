1766b4883034484bd8b363a2b0cafd3b05bfe79b
MartinCespedes <martin.cespedes002@mymdc.net>
2023-01-10 14:38:26 -0500

    Created Notes routes in server.js & added apiRoutes.js in routes folder

---
+++ Develop/routes/apiRoutes.js
@@ -0,0 +1,8 @@
+const { Router } = require("express");
+const { uuid } = require("crypto");
+
+app.get("notes/:id", (req, res) => {
+  const note = parsedNotes[req.params.id];
+});
+
+app.get("api", (req, res) => res.json(notes));
--- server.js
+++ server.js
@@ -5,8 +5,9 @@ const htmlRoutes = require("./routes/htmlRoutes");
 const apiRoutes = require("./routes/apiRoutes");
 
 // Require the JSON file and assign it to a variable called `termData`
+const { request } = require("http");
 const termData = require("./terms.json");
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 // Initialize our app variable by setting it to the value of express()
 const app = express();
@@ -16,6 +17,14 @@ app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
 app.use(express.static("public"));
+
+const parsedNotes = Json.parse(Notes);
+console.info("parsed notes", parsedNotes);
+
+// route for notes
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/notes.html"));
+});
 
 app.use(apiRoutes);
 app.use(htmlRoutes);
